74cd83c025da477aa3716de75fa26b3ae39816e1
Josh Klun <jklun@nerdery.com>
2016-06-08 11:20:02 -0500

    Add a variety of other tests for Entrant logic.

--- src/test/groovy/com/nerdery/jvm/resistance/test/EntrantSpec.groovy
+++ src/test/groovy/com/nerdery/jvm/resistance/test/EntrantSpec.groovy
@@ -1,6 +1,7 @@
 package com.nerdery.jvm.resistance.test
 
 import com.nerdery.jvm.resistance.tournament.Entrant
+import com.nerdery.jvm.resistance.tournament.EntrantGenerationResult
 import spock.lang.Specification
 
 /**
@@ -32,10 +33,91 @@ class EntrantSpec extends Specification {
         when: "entrants searched in #searchPackage"
         def entrants = finder.searchPackage(searchPackage).find()
 
-        then: "Test Bot should notbe in the list"
+        then: "Test Bot should not be in the list"
         println(entrants)
         entrants.find { entrant ->
             entrant.doctorBot.class.name == "com.nerdery.jvm.resistance.bots.test.TestBot"
         } == null
     }
+
+    def "Resistance Bot should always prescribe antibiotics"() {
+        given:
+        def finder = Entrant.finder()
+        def searchPackage = "com.nerdery.jvm.resistance.bots.test"
+
+        when: "entrants searched in #searchPackage"
+        def entrants = finder.searchPackage(searchPackage).find()
+
+        then: "Resistance bot should return true (prescribe antibiotics)"
+        println(entrants)
+        entrants.find { entrant ->
+            entrant.doctorBot.class.name == "com.nerdery.jvm.resistance.bots.test.ResistanceBot"
+        }.doctorBot.prescribeAntibiotic(2.0f, Collections.emptyList())
+    }
+
+    def "Malpractice Bot should never prescribe antibiotics"() {
+        given:
+        def finder = Entrant.finder()
+        def searchPackage = "com.nerdery.jvm.resistance.bots.test"
+
+        when: "entrants searched in #searchPackage"
+        def entrants = finder.searchPackage(searchPackage).find()
+
+        then: "Malpractice bot should return false (prescribe rest)"
+        println(entrants)
+        !entrants.find { entrant ->
+            entrant.doctorBot.class.name == "com.nerdery.jvm.resistance.bots.test.MalpracticeBot"
+        }.doctorBot.prescribeAntibiotic(200.0f, Collections.emptyList())
+    }
+
+    def "An entrant should compute its score properly"() {
+        given:
+        def finder = Entrant.finder()
+        def searchPackage = "com.nerdery.jvm.resistance.bots.test"
+
+        when: "using MalpracticeBot, but it shouldn't matter"
+        def entrant = finder.searchPackage(searchPackage)
+                .find()
+                .find { entrant ->
+                    entrant.doctorBot.class.name == "com.nerdery.jvm.resistance.bots.test.MalpracticeBot"
+                }
+        // Note: generation numbers aren't consistent
+        entrant.addGenerationResult(new EntrantGenerationResult(5, 5, 5, 5, 5, 5, false))
+        entrant.addGenerationResult(new EntrantGenerationResult(50, 5, 5, 5, 5, 5, false))
+        entrant.addGenerationResult(new EntrantGenerationResult(-30, 5, 5, 5, 5, 5, false))
+        entrant.addGenerationResult(new EntrantGenerationResult(0, 5, 5, 5, 5, 5, false))
+        entrant.addGenerationResult(new EntrantGenerationResult(-2, 5, 5, 5, 5, 5, false))
+        entrant.addGenerationResult(new EntrantGenerationResult(17, 5, 5, 5, 5, 5, false))
+        def correctScore = 40;
+
+        then: "the score should be #correctScore"
+        println(entrant)
+        entrant.score == correctScore
+    }
+
+    def "An entrant should know how many extinctions it caused"() {
+        given:
+        def finder = Entrant.finder()
+        def searchPackage = "com.nerdery.jvm.resistance.bots.test"
+
+        when: "using MalpracticeBot, but it shouldn't matter"
+        def entrant = finder.searchPackage(searchPackage)
+                .find()
+                .find { entrant ->
+                    entrant.doctorBot.class.name == "com.nerdery.jvm.resistance.bots.test.ResistanceBot"
+                }
+        // Note: generation numbers aren't consistent
+        entrant.addGenerationResult(new EntrantGenerationResult(5, 5, 5, 5, 5, 5, false))
+        entrant.addGenerationResult(new EntrantGenerationResult(50, 5, 5, 5, 5, 5, true))
+        entrant.addGenerationResult(new EntrantGenerationResult(-30, 5, 5, 5, 5, 5, true))
+        entrant.addGenerationResult(new EntrantGenerationResult(0, 5, 5, 5, 5, 5, true))
+        entrant.addGenerationResult(new EntrantGenerationResult(-2, 5, 5, 5, 5, 5, false))
+        entrant.addGenerationResult(new EntrantGenerationResult(17, 5, 5, 5, 5, 5, true))
+        def correctExctinctions = 4;
+
+        then: "the extinctions should be #correctExctinctions"
+        println(entrant)
+        entrant.extinctionsCaused == correctExctinctions
+    }
+
 }
