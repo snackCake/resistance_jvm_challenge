4bff038771ab08394a556336fe3808589a9594c3
Stephen Hopper <stephenmhopper@gmail.com>
2016-06-08 12:56:28 -0500

    Added functionality for tweeting results.

--- src/main/java/com/nerdery/jvm/resistance/bots/hopper/HopperBayesianBot.java
+++ src/main/java/com/nerdery/jvm/resistance/bots/hopper/HopperBayesianBot.java
@@ -2,7 +2,13 @@ package com.nerdery.jvm.resistance.bots.hopper;
 
 import com.nerdery.jvm.resistance.bots.DoctorBot;
 import com.nerdery.jvm.resistance.models.Prescription;
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.Authorization;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.Format;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -27,6 +33,12 @@ public class HopperBayesianBot implements DoctorBot {
     private static final int MONEY_ANTIBIOTIC_BAD = -100;
     private static final int MONEY_REST_GOOD = 1;
     private static final int MONEY_REST_BAD = -10;
+
+    private static final Twitter twitter;
+
+    static {
+        twitter = TwitterFactory.getSingleton();
+    }
 
     private Map<String, List<PrescriptionData>> prescriptionMap = new HashMap<>();
 
@@ -55,7 +67,36 @@ public class HopperBayesianBot implements DoctorBot {
         boolean prescribeDrugs = probMoneyIfPrescribeAntibiotic > probMoneyIfPrescribeRest;
         System.out.println("Prescribe antibiotics?: " + prescribeDrugs);
 
+        StatusUpdate status = buildStatus(patientTemperature, prescribeDrugs, probMoneyIfPrescribeAntibiotic, probMoneyIfPrescribeRest);
+        try {
+            twitter.updateStatus(status);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+
         return prescribeDrugs;
+    }
+
+    private StatusUpdate buildStatus(float temp, boolean prescribeDrugs, float chanceMoneyAnti, float chanceMoneyRest) {
+        String status;
+        String antiMoneyStr = format(chanceMoneyAnti * 100.0f);
+        String restMoneyStr = format(chanceMoneyRest * 100.0f);
+        if (prescribeDrugs) {
+            status = "Prescribed drugs to a patient with a fever of " + format(temp) +
+                    "F as there's a " + antiMoneyStr + "% chance I get $$$ from this (vs " + restMoneyStr + "%) #NerderyResistance";
+        } else {
+            status = "Told some fool to rest up because their fever was " + format(temp) +
+                    "F and there's a " + restMoneyStr + "% chance I get $$$ from this (vs " + antiMoneyStr + "%) #NerderyResistance";
+        }
+        System.out.println(status);
+
+        StatusUpdate update = new StatusUpdate(status);
+        update.setPossiblySensitive(false); //hilarious
+        return update;
+    }
+
+    private String format(float f) {
+        return String.format("%.1f", f);
     }
 
     private void updatePrescriptions(Collection<Prescription> previousPrescriptions) {
