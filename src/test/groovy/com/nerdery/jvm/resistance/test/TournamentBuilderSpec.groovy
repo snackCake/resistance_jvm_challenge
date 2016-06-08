7839704c709c180a632ad55a76ef2d408401955e
Josh Klun <jklun@nerdery.com>
2016-06-08 14:44:00 -0500

    Add a test spec for the tournament builder.

---
+++ src/test/groovy/com/nerdery/jvm/resistance/test/TournamentBuilderSpec.groovy
@@ -0,0 +1,26 @@
+package com.nerdery.jvm.resistance.test
+
+import com.nerdery.jvm.resistance.TournamentRunner
+import com.nerdery.jvm.resistance.models.tournament.Tournament
+import spock.lang.Specification
+
+/**
+ * @author Josh Klun (jklun@nerdery.com)
+ */
+class TournamentBuilderSpec extends Specification {
+
+    def "Built tournament will have the correct number of generations"() {
+        given:
+        def builder = Tournament.builder()
+        def correctEntrantCount = 6
+
+        when: "The entrant count is at the assumed #correctEntrantCount"
+        def tournament = builder.entrantPackage(TournamentRunner.TEST_ENTRANT_PACKAGE).build()
+        def correctGenerationCount = 15
+
+        then: "There will be #correctGenerationCount generations, to allow for all combinations"
+        println(tournament)
+        tournament.listScoredEntrants().size() == correctEntrantCount
+        tournament.generations.size() == correctGenerationCount
+    }
+}
