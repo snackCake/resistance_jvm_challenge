f75842bcf108d85b60470428466c1dc5ff5f3b4a
Josh Klun <jklun@nerdery.com>
2016-06-09 06:53:50 -0500

    Fix unit test to handle 7th test bot from Shopper.

--- src/test/groovy/com/nerdery/jvm/resistance/test/TournamentBuilderSpec.groovy
+++ src/test/groovy/com/nerdery/jvm/resistance/test/TournamentBuilderSpec.groovy
@@ -12,11 +12,11 @@ class TournamentBuilderSpec extends Specification {
     def "Built tournament will have the correct number of generations"() {
         given:
         def builder = Tournament.builder()
-        def correctEntrantCount = 6
+        def correctEntrantCount = 7
 
         when: "The entrant count is at the assumed #correctEntrantCount"
         def tournament = builder.entrantPackage(TournamentRunner.TEST_ENTRANT_PACKAGE).build()
-        def correctGenerationCount = 15
+        def correctGenerationCount = 35
 
         then: "There will be #correctGenerationCount generations, to allow for all combinations"
         println(tournament)
